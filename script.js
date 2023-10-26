cd17cb96a686eeb62cf3dc69862969673f5e1e51
Daniel Muñoz <dafemure.180@gmail.com>
2023-10-26 18:30:12 -0500

    Se estructura el codigo con funciones para un mejor manejo

--- script.js
+++ script.js
@@ -1,26 +1,125 @@
-document.getElementById("searchButton").addEventListener("click", function () {
+document.getElementById("searchButton").addEventListener("click", async function () {
     const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
 
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            const pokemonInfo = document.getElementById("pokemonInfo");
-            pokemonInfo.innerHTML = `
-                <h2>${data.name}</h2>
-                <img src="${data.sprites.front_default}" alt="${data.name}">
-                <p>Descripción: ${data.species.name}</p>
-                <p>Habilidades: ${data.abilities.map((ability) => ability.ability.name).join(', ')}</p>
-            `;
-            if (data.evolutions && data.evolutions.length > 0) {
-                pokemonInfo.innerHTML += `<button id="evolutionButton">Evolucionar</button>`;
-                document.getElementById("evolutionButton").addEventListener("click", function () {
-                    window.location.href = `evolution.html?pokemon=${data.evolutions[0].name}`;
+    //Consumo la funcion que extrae la info basica del pokemon en base al nombre
+    const data = await extractPokemodDetails(pokemonName)
+    //Consumo la funcion para extraer la descripcion y la url de evolucion
+    const dataDescription = await getPokemonDescription(data.idPokemon)
+    //Consumo la tercer funcion para obtener las posibles evoluciones del pokemon
+    const pokemonEvolutions = await getEvolutions(dataDescription.evolutionChain)
+
+    //Pintamos en el front la informacion basica extraida del pokemon
+    pokemonInfo.innerHTML = `
+        <h2>${data.name}</h2>
+        <img src="${data.img}" alt="${data.name}">
+        <p>Descripción: ${dataDescription.flavorText}</p>
+        <p>Habilidades: ${data.abilities.map((ability) => ability.ability.name).join(', ')}</p>`;
+
+    console.log(pokemonEvolutions)
+
+
+    if (data.evolutions && data.evolutions.length > 0) {
+        pokemonInfo.innerHTML += `<button id="evolutionButton">Evolucionar</button>`;
+        document.getElementById("evolutionButton").addEventListener("click", function () {
+            window.location.href = `evolution.html?pokemon=${data.evolutions[0].name}`;
+        });
+    }
+    
+});
+
+
+//Funcion para obtener la informacion basica del pokemon
+async function extractPokemodDetails(pokemonName){
+    try{
+
+        //se realiza el consumo de la API principal
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const pokemonData = response.data;
+
+        // Extraigo los datos de interés
+        const data = {
+            name: pokemonData.name,
+            abilities: pokemonData.abilities,
+            img: pokemonData.sprites.other['official-artwork'].front_default,
+            species: pokemonData.species,
+            idPokemon: pokemonData.id
+        };
+
+        //retorno lo datos extraido
+        return data
+    }catch(error){
+        console.log('Error al obtener el pokemon ' + error)
+        const pokemonInfo = document.getElementById("pokemonInfo");
+        pokemonInfo.innerHTML = "¡Pokémon no encontrado!";
+    }
+}
+
+//Funcion para obtener la descripcion del pokemon en español y la url de la cadena de evolucion
+async function getPokemonDescription(id_pokemon){
+    try{
+        // Consumo el API de species para obtener descripción y URL de cadena de evolución
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id_pokemon}`);
+
+        // Depuración necesaria para ir entendiendo y saber cómo manejar los datos que recibes
+        console.log(`La petición a la API se completó correctamente con status: ${response.status}`);
+
+        // Filtra el primer flavor text en inglés
+        const flavorTextEntries = response.data['flavor_text_entries'];
+        let spanishFlavorText = '';
+        for (const entry of flavorTextEntries) {
+            if (entry.language.name === 'es') {
+                spanishFlavorText = entry.flavor_text;
+                break; // Al encontrar el primer entry en ingles, lo extraigo
+            }
+        }
+
+        // Extraigo los datos de interés
+        const data = {
+            flavorText: spanishFlavorText,
+            evolutionChain: response.data['evolution_chain']['url'],
+        };
+
+        // Los retorno de la función
+        return data;
+
+    } catch(error){
+        //Mostrar error de campo vacio
+        console.error(`fallo la petición a la api con error: ${error.message}`);
+    }    
+}
+
+async function getEvolutions(evolutionChainUrl) {
+    try {
+        //Consumo el API de de cadena de evolucion
+        const response = await axios.get(evolutionChainUrl);
+
+        //depuracion necesaria para ir entendiendo y saber como manejar los datos que recibia 
+        console.log(`La petición a la cadena de evolución se completó correctamente con status: ${response.status}`);
+        
+        //Extraer el arreglo con la cadena de evolucion
+        const chainData = response.data.chain;
+        const evolutions = [];
+        
+        //Funcion para extraer los nombres de los pokemons en orden de evolucion
+        function extractEvolutions(chain) {
+            if (chain.species && chain.species.name) {
+                evolutions.push(chain.species.name);
+            }
+            if (chain.evolves_to && chain.evolves_to.length > 0) {
+                chain.evolves_to.forEach(subchain => {
+                    extractEvolutions(subchain);
                 });
             }
-        })
-        .catch((error) => {
-            const pokemonInfo = document.getElementById("pokemonInfo");
-            pokemonInfo.innerHTML = "¡Pokémon no encontrado!";
-        });
-});
+        }
+        
+        //Ejecutamos la funcion
+        extractEvolutions(chainData);
+        
+        //Retorno el arreglo con la lista ordenada de evolucion del pokemon
+        return evolutions;
+
+    } catch (error) {
+        //Mostrar error de campo vacio
+        console.error(`fallo la petición a la api con error: ${error.message}`);
+    }
+}
